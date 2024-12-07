3374e50c110d757762149ef81bc10155c3f67641
bonzo4 <alonzo.sabino814@gmail.com>
2024-12-07 14:19:18 -0600

    fix: description verbage

--- src/components/About/Description.tsx
+++ src/components/About/Description.tsx
@@ -3,9 +3,9 @@ export default function Description() {
     <div className="bg-w flex w-full flex-col items-start justify-start gap-4 lg:w-1/2">
       <h2 className="text-xl lg:text-3xl">Description:</h2>
       <p className="text-base lg:text-xl">
-        Hello, I&apos;m Alonzo, a passionate software developer with expertise
-        in building Web3 platforms. I have proven track record in crafting
-        high-engagement newsletters, building responsive UIs, and creating
+        Hello, I&apos;m Alonzo, a passionate software developer with experience
+        in building Solana platforms. I have proven track record in building
+        high-engagement newsletters, crafting responsive UIs, and creating
         robust blockchain applications using Typescript, Rust, and Open Source
         Technology.
       </p>
